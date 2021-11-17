a538d372224326e554b572bc892a3dc5489cd0e4
Shoeb Merchant <shoebmerchant12@gmail.com>
2021-11-17 20:10:32 +0530

    completed

---
+++ build/precache-manifest.632a652144efce035bcdd86a71f35888.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a62fe7e9fd1a96a50f147d3ba71b1d",
+    "url": "/To-Do-List-React/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/To-Do-List-React/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "283f13a475ebc648f047",
+    "url": "/To-Do-List-React/static/js/main.74d42e5e.chunk.js"
+  },
+  {
+    "revision": "14838dc6d404cad54a09",
+    "url": "/To-Do-List-React/static/js/runtime-main.b14e0f21.js"
+  }
+]);
